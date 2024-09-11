eef460eb4809bb1d62f58f4cfd3ddbd422a4ccb3
mdesanic21 <mdesanic21@foi.hr>
2024-09-11 10:51:50 +0200

    First lectures.

---
+++ 01-first-steps/first-php-file.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo 'Hello world!';
+    ?>
+    <h1>Hey, this is my first .php file</h1>
+    <h1><?php echo rand(1,10); ?></h1>
+</body>
+</html>
